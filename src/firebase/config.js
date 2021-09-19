013e49fe9493f03c885c44ca5085610452fd6678
Kadir Zukorlic <kadirzukorlic@gmail.com>
2021-09-20 00:11:38 +0200

    firebase config, auth started

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDirWxo_yqnBlOGUeMl8SRfwj9nndGM3w0",
+    authDomain: "e-commerce-app-18f34.firebaseapp.com",
+    projectId: "e-commerce-app-18f34",
+    storageBucket: "e-commerce-app-18f34.appspot.com",
+    messagingSenderId: "1099493251784",
+    appId: "1:1099493251784:web:8a05ff78f96e67536923fd",
+    measurementId: "G-Y5WQT1KP3F"
+  };
