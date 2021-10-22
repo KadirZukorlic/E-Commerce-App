cf7f8b402e735d8e432e439db66c1de6b1ab76dc
Kadir Zukorlic <kadirzukorlic@gmail.com>
2021-10-22 12:35:45 +0200

    pwd validation

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import { auth, handleUserProfile } from "./../../firebase/utils";
+
 import FormInput from "../Forms/FormInput";
 import Button from "../Forms/Button/";
 
@@ -10,6 +12,7 @@ const initialState = {
   email: "",
   password: "",
   confirmPassword: "",
+  errors: []
 };
 
 class SignUp extends Component {
@@ -28,41 +31,75 @@ class SignUp extends Component {
     });
   }
 
+  handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const { displayName, email, password, confirmPassword, errors } = this.state;
+
+    if (password !== confirmPassword) {
+      const err = ['Password Don\'t match'];
+      this.setState({
+        errors: err
+      })
+      return ;
+    }
+  };
+
   render() {
-    const { displayName, email, password, confirmPassword } = this.state;
+    const { displayName, email, password, confirmPassword, errors } = this.state;
 
     return (
       <div className="signup">
         <div className="wrap">
           <h2>Sign Up</h2>
-          <form>
-            {displayName}
-            <FormInput
-              type="text"
-              name="displayName"
-              value={displayName}
-              placeholder="full name"
-              onChange={this.handleChange}
-            />
-            <FormInput
-              type="password"
-              name="password"
-              value={password}
-              placeholder="Password"
-              onChange={this.handleChange}
-            />
-            <FormInput
-              type="password"
-              name="confirmPassword"
-              value={confirmPassword}
-              placeholder="Confirm Password"
-              onChange={this.handleChange}
-            />
 
-            <Button type="submit">
-                Register
-            </Button>
-          </form>
+          {errors.length > 0 && (
+            <ul>
+              {errors.map((error, index) => {
+                return (
+                  <li key={index}>
+                  {error}
+                </li>
+                )
+  
+              })}
+            </ul>
+          )}
+
+          <div className="formWrap">
+            <form onSubmit={this.handleFormSubmit}>
+              {displayName}
+              <FormInput
+                type="text"
+                name="displayName"
+                value={displayName}
+                placeholder="Full Name"
+                onChange={this.handleChange}
+              />
+              <FormInput
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Email"
+                onChange={this.handleChange}
+              />
+              <FormInput
+                type="password"
+                name="password"
+                value={password}
+                placeholder="Password"
+                onChange={this.handleChange}
+              />
+              <FormInput
+                type="password"
+                name="confirmPassword"
+                value={confirmPassword}
+                placeholder="Confirm Password"
+                onChange={this.handleChange}
+              />
+
+              <Button type="submit">Register</Button>
+            </form>
+          </div>
         </div>
       </div>
     );
