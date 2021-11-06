c9827002d36f94e28530b3ddbb38419f8c3b15ea
Kadir Zukorlic <kadirzukorlic@gmail.com>
2021-11-07 00:49:13 +0100

    handling signIn user with redux

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,37 +1,47 @@
-import React, { useState } from "react";
-import { Link, withRouter } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
+import { signInUser } from './../../redux/User/user-actions';
 
-import Button from "./../Forms/Button/index";
-import { signInWithGoogle, auth } from "./../../firebase/utils";
-import AuthWrapper from "../AuthWrapper";
+import Button from './../Forms/Button/index';
+import { signInWithGoogle, auth } from './../../firebase/utils';
+import AuthWrapper from '../AuthWrapper';
 
-import FormInput from "../Forms/FormInput";
+import FormInput from '../Forms/FormInput';
 
-import "./styles.scss";
+import './styles.scss';
 
-const SignIn = props => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const mapState = ({ user }) => ({
+  signInSuccess: user.signInSuccess,
+});
+
+const SignIn = (props) => {
+  const dispatch = useDispatch();
+  const { signInSuccess } = useSelector(mapState);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  console.log(signInSuccess);
+
+  useEffect(() => {
+    if (signInSuccess) {
+      resetForm();
+      props.history.push('/');
+    }
+  }, [signInSuccess]);
 
   const resetForm = () => {
-    setEmail("");
-    setPassword("");
+    setEmail('');
+    setPassword('');
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      resetForm();
-      props.history.push("/");
-    } catch (error) {
-      console.log(error);
-    }
+    await dispatch(signInUser({ email, password }));
   };
 
   const configAuthWrapper = {
-    headline: "LogIn",
+    headline: 'LogIn',
   };
 
   return (
