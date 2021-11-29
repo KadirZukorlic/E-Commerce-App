78d32a20ba6ffcffca59ca0ac066f72649a1fbef
Kadir Zukorlic <kadirzukorlic@gmail.com>
2021-11-29 22:01:36 +0100

    making admin page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Switch, Route } from "react-router-dom";
-import { checkUserSession } from "./redux/User/user-actions";
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import { checkUserSession } from './redux/User/user-actions';
 
 // components
-import AdminToolbar from "./components/AdminToolbar";
+import AdminToolbar from './components/AdminToolbar';
 
 // hoc
-import WithAuth from "./hoc/withAuth";
-import WithAdminAuth from "./hoc/withAdminAuth";
+import WithAuth from './hoc/withAuth';
+import WithAdminAuth from './hoc/withAdminAuth';
 
 // layouts
-import MainLayout from "./components/layouts/MainLayout";
-import HomepageLayout from "./components/layouts/HomepageLayout";
+import AdminLayout from './components/layouts/AdminLayout';
+import MainLayout from './components/layouts/MainLayout';
+import HomepageLayout from './components/layouts/HomepageLayout';
 
 // pages
-import Admin from "./pages/Admin";
-import Homepage from "././pages/Homepage";
-import Registration from "./pages/Registration";
-import Login from "./pages/Login";
-import Recovery from "./pages/Recovery";
-import Dashboard from "./pages/Dashboard";
+import Admin from './pages/Admin';
+import Homepage from '././pages/Homepage';
+import Registration from './pages/Registration';
+import Login from './pages/Login';
+import Recovery from './pages/Recovery';
+import Dashboard from './pages/Dashboard';
 
-import "./default.scss";
+import './default.scss';
 
 const App = (props) => {
   const dispatch = useDispatch();
@@ -89,9 +90,9 @@ const App = (props) => {
           path="/admin"
           render={() => (
             <WithAdminAuth>
-              <MainLayout>
+              <AdminLayout>
                 <Admin />
-              </MainLayout>
+              </AdminLayout>
             </WithAdminAuth>
           )}
         />
---
+++ src/components/VerticalNav/index.js
@@ -0,0 +1,17 @@
+import React, { Children } from 'react';
+
+import './styles.scss';
+
+const VerticalNav = () => {
+    return (
+        <div className="verticalNav">
+
+            <div className="menu">
+                {Children}
+            </div>
+            
+        </div>
+    )
+}
+
+export default VerticalNav;
