2fc9a555fd383eefe26f07190b2093433a013e28
Kadir Zukorlic <kadirzukorlic@gmail.com>
2021-11-30 23:33:36 +0100

    design finished

--- src/components/VerticalNav/index.js
+++ src/components/VerticalNav/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import UserProfile from '../UserProfile';
 
 import './styles.scss';
 
@@ -10,8 +11,14 @@ const mapState = ({user}) => ({
 const VerticalNav = ({children}) => {
     const {currentUser} = useSelector(mapState)
 
+    const configUserProfile = {
+        currentUser
+    }
+
     return (
         <div className="verticalNav">
+
+            <UserProfile {...configUserProfile}/>
 
             <div className="menu">
                 {children}
