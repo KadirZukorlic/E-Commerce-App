6f90301e4d7268999e4191d6ac1ba22edac1f3fa
Kadir Zukorlic <kadirzukorlic@gmail.com>
2021-12-20 16:11:56 +0100

    save order

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import { useSelector } from "react-redux";
-import { selectCartItems, selectCartTotal } from "./../../redux/Cart/cart-selectors";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "./../../redux/Cart/cart-selectors";
 import { createStructuredSelector } from "reselect";
+import { saveOrderHistory } from "./../../redux/Orders/orders-actions";
 
-import Item from './Item';
+import Item from "./Item";
 import Button from "../Forms/Button";
 
 import "./styles.scss";
@@ -15,14 +19,41 @@ const mapState = createStructuredSelector({
 });
 
 const Checkout = ({}) => {
-  const history = useHistory()
+  const dispatch = useDispatch();
+  const history = useHistory();
   const { cartItems, total } = useSelector(mapState);
+  const [order, setOrder] = useState({});
+
+  console.log(cartItems, "CART ITEMS");
+
+  const errMsg = "You have no items in Your cart.";
+
+  //   if (!Array.isArray(cartItems) || cartItems.length < 1) return null;
   
-  console.log(cartItems, 'CART ITEMS')
+  const configOrder = {
+    orderTotal: total,
+    orderItems: cartItems.map((item) => {
+      const {
+        documentID,
+        productThumbnail,
+        productName,
+        productPrice,
+        quantity,
+      } = item;
 
-  const errMsg = 'You have no items in Your cart.'
+      return {
+        documentID,
+        productThumbnail,
+        productName,
+        productPrice,
+        quantity,
+      };
+    }),
+  };
 
-//   if (!Array.isArray(cartItems) || cartItems.length < 1) return null;
+  const handleSaveOrder = () => {
+    dispatch(saveOrderHistory(configOrder))
+  };
 
   return (
     <div className="checkout">
@@ -30,77 +61,78 @@ const Checkout = ({}) => {
 
       <div className="cart">
         {cartItems.length > 0 ? (
-        <table border="0" cellPadding="0" cellSpacing="0">
-        <tbody>
-          <tr>
-            <table
-              className="checkoutHeader"
-              border="0"
-              cellPadding="10"
-              cellSpacing="0"
-            >
-              <tbody>
-                <tr>
-                  <th>Product</th>
-                  <th>Description</th>
-                  <th>Quantity</th>
-                  <th>Price</th>
-                  <th>Remove</th>
-                </tr>
-              </tbody>
-            </table>
-          </tr>
-
-          <tr>
-            <table border="0" cellSpacing="0" cellPadding="0">
-              <tbody>
-                {cartItems.map((item, pos) => {
-                  console.log(item, 'ITEM PRODUCT')
-                  return (
-                    <tr key={pos}>
-                      <td><Item {...item}/></td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </tr>
-
-          <tr>
-            <table align="right" border="0" cellSpacing="0" cellPading="10">
-              <tr align="right">
-                <td>
-                  <h3>Total: €{total}</h3>
-                </td>
-              </tr>
-
+          <table border="0" cellPadding="0" cellSpacing="0">
+            <tbody>
               <tr>
-                <table border="0" cellPadding="10" cellSpacing="0">
+                <table
+                  className="checkoutHeader"
+                  border="0"
+                  cellPadding="10"
+                  cellSpacing="0"
+                >
                   <tbody>
                     <tr>
-                      <td>
-                        <Button onClick={() => history.goBack()}>Continue Shopping</Button>
-                      </td>
-                      <td>
-                        <Button>Checkout</Button>
-                      </td>
+                      <th>Product</th>
+                      <th>Description</th>
+                      <th>Quantity</th>
+                      <th>Price</th>
+                      <th>Remove</th>
                     </tr>
                   </tbody>
                 </table>
               </tr>
-            </table>
-          </tr>
-        </tbody>
-      </table>
-        ): (
-            <p>
-                {errMsg}
-            </p>
+
+              <tr>
+                <table border="0" cellSpacing="0" cellPadding="0">
+                  <tbody>
+                    {cartItems.map((item, pos) => {
+                      console.log(item, "ITEM PRODUCT");
+                      return (
+                        <tr key={pos}>
+                          <td>
+                            <Item {...item} />
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </tr>
+
+              <tr>
+                <table align="right" border="0" cellSpacing="0" cellPading="10">
+                  <tr align="right">
+                    <td>
+                      <h3>Total: €{total}</h3>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <table border="0" cellPadding="10" cellSpacing="0">
+                      <tbody>
+                        <tr>
+                          <td>
+                            <Button onClick={() => history.goBack()}>
+                              Continue Shopping
+                            </Button>
+                          </td>
+                          <td>
+                            <Button onClick={() => handleSaveOrder(configOrder)}>
+                              Checkout
+                            </Button>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </tr>
+                </table>
+              </tr>
+            </tbody>
+          </table>
+        ) : (
+          <p>{errMsg}</p>
         )}
-
-
       </div>
-
     </div>
   );
 };
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,5 +5,6 @@ export const firebaseConfig = {
     storageBucket: "e-commerce-app-18f34.appspot.com",
     messagingSenderId: "1099493251784",
     appId: "1:1099493251784:web:8a05ff78f96e67536923fd",
-    measurementId: "G-Y5WQT1KP3F"
+    measurementId: "G-Y5WQT1KP3F",
+    databaseURL: 'https://e-commerce-app-18f34-default-rtdb.firebaseio.com/',
   };
